1a0333f0056e0d5dc3431da6c6dd221c33bd5e22
msaad-1234 <122250808+msaad-1234@users.noreply.github.com>
2023-01-10 12:11:26 +0500

    Update example.java

--- example.java
+++ example.java
@@ -11,7 +11,7 @@ public class TestThrow {
     //main method  
     public static void main(String[] args) {  
             TestThrow obj = new TestThrow();  
-            obj.checkNum(9);  
+            obj.checkNum(-34);  
             System.out.println("Rest of the code..");  
     }  
 } 
